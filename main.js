02c51c138e955c66ee35f0e85c396e2473abe37d
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-01-31 14:06:23 +0300

    connected swiper

---
+++ main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  effect: 'slide',
+  speed: 500,
+  autoplay: true,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
