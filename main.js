0f6a41947b139eb87aaba7516619d275a88e2bb5
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-02-04 15:52:03 +0300

    initial markup burger menu

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+// Подключение Burger Menu //
+
+// function burgerMenu() {
+//   let burger = document.querySelector(".burger");
+
+//   burger.addEventListener('click', function () {
+//       burger.classList.toggle("burger_active");
+//   })
+
+// }
+
+// burgerMenu();
+
+function burgerMenu() {
+  let burger_icon = document.querySelector(".burger_icon");
+  let burger_menu = document.getElementById("burger_menu");
+  let close_icon = document.getElementById("close_icon");
+
+  burger_icon.addEventListener('click', function() {
+      burger_menu.classList.toggle("burger_menu_active");
+  })
+  close_icon.addEventListener('click', function() {
+      burger_menu.classList.toggle("burger_menu_active");
+  })
+
+}
+burgerMenu()
+
+// Подключение Swiper //
 const swiper = new Swiper('.swiper', {
 
   // Optional parameters
@@ -14,5 +43,7 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+
+
 
 
