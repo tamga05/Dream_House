f77b10e61da0dc6f6c4201f25efa594908c9a7a0
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-02-04 22:14:32 +0300

    markup burger menu

--- main.js
+++ main.js
@@ -1,49 +1,33 @@
 // Подключение Burger Menu //
 
-// function burgerMenu() {
-//   let burger = document.querySelector(".burger");
-
-//   burger.addEventListener('click', function () {
-//       burger.classList.toggle("burger_active");
-//   })
-
-// }
-
-// burgerMenu();
-
 function burgerMenu() {
   let burger_icon = document.querySelector(".burger_icon");
   let burger_menu = document.getElementById("burger_menu");
   let close_icon = document.getElementById("close_icon");
 
-  burger_icon.addEventListener('click', function() {
-      burger_menu.classList.toggle("burger_menu_active");
-  })
-  close_icon.addEventListener('click', function() {
-      burger_menu.classList.toggle("burger_menu_active");
-  })
-
+  burger_icon.addEventListener("click", toggleMenu);
+  close_icon.addEventListener("click", toggleMenu);
 }
-burgerMenu()
+
+burgerMenu();
+
+function toggleMenu() {
+  burger_menu.classList.toggle("burger_menu_active");
+}
 
 // Подключение Swiper //
-const swiper = new Swiper('.swiper', {
 
+const swiper = new Swiper(".swiper", {
   // Optional parameters
-  direction: 'horizontal',
-  effect: 'slide',
+  direction: "horizontal",
+  effect: "slide",
   speed: 300,
   autoplay: true,
   loop: true,
 
   // Navigation buttons
   navigation: {
-    prevEl: '.swiper__btn-prev',
-    nextEl: '.swiper__btn-next',
+    prevEl: ".swiper__btn-prev",
+    nextEl: ".swiper__btn-next",
   },
-
 });
-
-
-
-
